0fe2b638fb1661c989bf3b15f19c88e82376b45c
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2019-06-01 15:45:59 -0700

    Adding buttons in Index

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="index">
     <h1>Index</h1>
+    <router-link to="/signup" tag="button">SignUp</router-link> |
+    <router-link to="/login" tag="button">LogIn</router-link>
   </div>
 </template>
