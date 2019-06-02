dc5cf316b753e366d39225966e649eb0aa4d2661
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2019-06-01 18:54:15 -0700

    Add store
    `$ vue add store`

--- src/main.js
+++ src/main.js
@@ -7,11 +7,13 @@ Vue.config.productionTip = false
 import Amplify, * as AmplifyModules from 'aws-amplify'
 import { AmplifyPlugin } from 'aws-amplify-vue'
 import awsconfig from './aws-exports'
+import store from './store'
 Amplify.configure(awsconfig)
 
 Vue.use(AmplifyPlugin, AmplifyModules)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
