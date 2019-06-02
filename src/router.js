3abdc2285160a7c17b3b476a9ed664206f587baf
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2019-06-01 18:13:53 -0700

    Remove unused files

--- src/router.js
+++ src/router.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import Index from './views/Index.vue'
-import SignUp from './views/SignUp.vue'
-import LogIn from './views/LogIn.vue'
 
 Vue.use(Router)
 
